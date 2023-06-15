d5dad375d039597bec7a41f62506b4526387ea06
sey323 <sey323_nm@yahoo.co.jp>
2023-06-16 00:23:18 +0900

    :recycle: ファイル名修正

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -4,13 +4,23 @@ from app.master.dataset.models.entry_dataset import (
     EntryDatasetResponse,
 )
 from app.main import app
+from app.master.dataset.models.purchase_dataset import (
+    PurchaseDatasetRequest,
+    PurchasedDatasetResponse,
+)
 
 client = TestClient(app)
 
 
-def test_entry_dataset_endpoint():
+def test_entry_dataset_endpoint(mocker):
+    # generate_id_strで固定の値を返すmockを作成
+    test_dataset_id = "12345"
+    mocker.patch(
+        "app.master.dataset.services.entry_dataset_service.generate_id_str",
+        return_value=test_dataset_id,
+    )
+
     request_payload = {"user_id": "sample data", "description": "sample name"}
-    # ダミーのzipファイルを作成
 
     # Make a request to the endpoint with the sample payload
     response = client.post(
@@ -35,8 +45,28 @@ def test_entry_dataset_endpoint():
 
 def test_list_dataset():
     test_entry_dataset_endpoint()
+
+    # Make a request to the endpoint with the sample payload
     response = client.get("/dataset")
+
+    # then
     assert response.status_code == 200
     datasets = response.json()["datasets"]
     assert isinstance(datasets, list)
     assert len(datasets) >= 1
+
+
+def test_purchase_dataset(mocker):
+    test_entry_dataset_endpoint(mocker)
+
+    test_dataset_id = "12345"
+    request = PurchaseDatasetRequest(
+        user_id="sample_user_id",
+    )
+
+    # Send a POST request to the API endpoint
+    response = client.post(f"/dataset/{test_dataset_id}/purchased", data=request.json())
+
+    # Assert that the response status code is 200
+    assert response.status_code == 200
+    assert response.json()["dataset_id"] == test_dataset_id
