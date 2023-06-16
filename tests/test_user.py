3968f864b5bd984978ba0ea93c9f9344e7027677
sey323 <sey323_nm@yahoo.co.jp>
2023-06-16 20:24:44 +0900

    :recycle: テスト用のユーザidを変更

--- tests/test_user.py
+++ tests/test_user.py
@@ -10,12 +10,12 @@ client = TestClient(app)
 def test_signup_not_exists(mocker):
     """ユーザが登録済みでない場合、新規登録ができること"""
     # give
-    test_user_id = "test_user_id"
+    sample_user_id = "sample_user_id"
     mocker.patch(
         "app.master.user.services.entry_user_service.generate_id_str",
-        return_value=test_user_id,
+        return_value=sample_user_id,
     )
-    user_store.delete_user(test_user_id)  # 削除してから実行する
+    user_store.delete_user(sample_user_id)  # 削除してから実行する
 
     response = client.post(
         "/signup",
@@ -26,17 +26,17 @@ def test_signup_not_exists(mocker):
     )
 
     assert response.status_code == 200
-    assert response.json() == {"user_id": f"{test_user_id}"}
+    assert response.json() == {"user_id": f"{sample_user_id}"}
 
 
 def test_signup_exists(mocker):
     """ユーザが登録済みの場合、そのユーザのIDを返すこと"""
     test_signup_not_exists(mocker=mocker)
     # give
-    test_user_id = "test_user_id"
+    sample_user_id = "sample_user_id"
     mocker.patch(
         "app.master.user.services.entry_user_service.generate_id_str",
-        return_value=test_user_id,
+        return_value=sample_user_id,
     )
 
     response = client.post(
@@ -48,13 +48,13 @@ def test_signup_exists(mocker):
     )
 
     assert response.status_code == 200
-    assert response.json() == {"user_id": f"{test_user_id}"}
+    assert response.json() == {"user_id": f"{sample_user_id}"}
 
 
 def test_login_wallet_address(mocker):
     # test_signup_not_exists(mocker)
     # give
-    test_user_id = "test_user_id"
+    sample_user_id = "sample_user_id"
     test_wallet_address = "0xb872960EF2cBDecFdC64115E1C77067c16f042FB"
 
     response = client.get(
@@ -63,5 +63,5 @@ def test_login_wallet_address(mocker):
 
     assert response.status_code == 200
     actual_user = User.parse_obj(response.json())
-    assert actual_user.user_id == test_user_id
+    assert actual_user.user_id == sample_user_id
     assert actual_user.wallet_address == test_wallet_address
