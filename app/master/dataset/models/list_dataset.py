82a7a25446f7c82303e08b7f004f6eb4eba18b7d
Ichirou.S <sey323@users.noreply.github.com>
2023-06-17 15:05:13 +0900

    :sparkles: 一覧取得時にユーザのデータセットに対する状態を取得 (#27)

--- app/master/dataset/models/list_dataset.py
+++ app/master/dataset/models/list_dataset.py
@@ -1,8 +1,20 @@
+from enum import Enum
 from typing import List
 from pydantic import BaseModel, Field
 
 from app.master.dataset.models.domain import Dataset
 
 
+class DatasetOwnType(str, Enum):
+    UNKNOWN = "unknown"  # ユーザIDを取得できないため不明
+    PURCHASED = "purchased"  # 自身の提案でなく購入済みのもの
+    UN_PURCHASED = "un_purchased"  # 自身の提案でなく購入済みでないもの
+    OWNER = "owner"  # 所有者
+
+
+class ListDatasetDto(Dataset):
+    dataset_own_type: DatasetOwnType = Field(DatasetOwnType)
+
+
 class ListDatasetResponse(BaseModel):
-    datasets: List[Dataset] = Field([], description="データセット一覧")
+    datasets: List[ListDatasetDto] = Field([], description="データセット一覧")
--- app/master/dataset/services/list_dataset_service.py
+++ app/master/dataset/services/list_dataset_service.py
@@ -1,7 +1,38 @@
-from app.facades.database import dataset_store
+from app.facades.database import dataset_store, user_store
+from app.master.dataset.models.list_dataset import DatasetOwnType, ListDatasetDto
 
 
-async def execute():
+async def execute(
+    user_id: str | None = None,
+):
     # FireStoreからDatasetの一覧を取得
     datasets = dataset_store.find_datasets()
-    return datasets
+
+    responses = []
+    for dataset in datasets:
+        list_dataset_dto = ListDatasetDto.parse_obj(dataset.dict())
+        if user_id is None:
+            list_dataset_dto.dataset_own_type = DatasetOwnType.UNKNOWN
+        elif user_id == dataset.user_id:
+            list_dataset_dto.dataset_own_type = DatasetOwnType.OWNER
+        elif _user_purchased(dataset_id=dataset.dataset_id, user_id=user_id):
+            list_dataset_dto.dataset_own_type = DatasetOwnType.PURCHASED
+        else:
+            list_dataset_dto.dataset_own_type = DatasetOwnType.UN_PURCHASED
+
+        responses.append(list_dataset_dto)
+    return responses
+
+
+def _user_purchased(dataset_id: str, user_id: str):
+    """ユーザが投票済みの場合Trueを返す"""
+    user = user_store.fetch_user(user_id)
+    own_dataset = [
+        dataset
+        for dataset in user.purchase_datasets
+        if dataset.dataset_id == dataset_id
+    ]
+    if own_dataset != []:  # 購入済み場合アクセス可能
+        return True
+    else:
+        return False
