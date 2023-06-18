81ece35177e67166495b51be33a651d152c6dcea
sey323 <sey323_nm@yahoo.co.jp>
2023-06-18 14:39:15 +0900

    :sparkles: Pythonからコントラクト実行処理

---
+++ app/facades/web3/account.py
@@ -0,0 +1,44 @@
+import os
+import secrets
+
+from eth_account import Account
+
+from app.utils.logging import logger
+
+
+class ContractOwner:
+    """スマートコントラクトを実行するウォレット"""
+
+    def __init__(self, private_key_filename: str) -> None:
+        """初期化処理
+
+        Args:
+            private_key_filename (str): 秘密鍵が保存されているパス。存在しない場合はそのパスに作成される。
+        """
+        self.private_key = self._load_private_key(private_key_filename)
+        _account = Account.from_key(self.private_key)
+        self.address = _account.address
+        print(f"Wallet Address: {self.address}")
+
+    @staticmethod
+    def _load_private_key(filename: str):
+        """秘密鍵を読み込む。存在しない場合は生成する。
+
+        Args:
+            filename (str): 秘密鍵が保存されているパス。存在しない場合はそのパスに作成される。
+
+        Returns:
+            _type_: _description_
+        """
+        if os.path.exists(filename):
+            # 秘密鍵の読み込み
+            with open(filename, "r") as fpr:
+                private_key = fpr.read()
+        else:
+            # 存在しない場合は秘密鍵を生成する
+            logger.info(f"private key is not exists. create key. : {filename}")
+            private_key = "0x" + secrets.token_hex(32)
+
+            with open(filename, "w") as fpr:
+                fpr.write(private_key)
+        return private_key
